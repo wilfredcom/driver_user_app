40f0a90ad95bedde59c61db6312f31b628631cc9
juliot nuñez <julio@nexia.io>
2022-03-17 20:11:44 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'drive_app_user',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
